3f7d72e40d6d10197a7acb7a9ee8486a7ed34d8f
aidgamesa <ya.ivanov1302@yandex.ru>
2022-10-31 04:53:20 +0000

    update

--- .package-env.sh
+++ .package-env.sh
@@ -1,33 +1,71 @@
 #!/usr/bin/env bash
 
-packagesdb="/usr/ports/packages.db"
+#packagesdb="/usr/ports/packages.db"
+packagesdb="../packages.db"
+PORTDIR=$PWD
+
+
 deps=()
 
 . $@
 
 #HELPERS
+init_db(){
+    touch $packagesdb
+}
+
 recreate_dir() {
     [ -d $1 ] && rm -rf $1
     mkdir $1
 }
 
+get_package_in_db() {
+    local port=$1
+    local version=${2:-}
+    grep "$1 $2" $packagesdb -m1 | cut -f2 -d' '
+}
+
+err() {
+    echo "[ERROR] $2"
+    exit $1
+}
+log() {
+    echo "[LOG] $1"
+}
+
+#ERRORS
+installed_error(){
+    err 1 "Package '$port' installed!"
+}
+
 #CODE
 install_deps() {
     for dep in "${deps[@]}"; do
-        echo "$port - $dep"
+        if [ -z "$(get_package_in_db $dep)" ]; then
+            cd "${PORTDIR}/../$dep"
+            ./package.sh
+        fi
     done
+    cd $PORTDIR
 }
 prepare_dirs() {
     recreate_dir "build"
 }
-
 download_source() {
     wget $source_code -O $port-$version.tar.xz
     cd build
     tar xf ../$port-$version.tar.xz
 }
+
+init_db
+[ -n "$(get_package_in_db $port)" ] && installed_error
+log "Preparing to build $port"
 install_deps
 prepare_dirs
-echo $(basename $PWD)
-echo $(basename $0)
+echo "$port $version" >> $packagesdb
 #download_source
+log "Building $port"
+build
+log "Installing $port"
+install
+log "Finish!"
