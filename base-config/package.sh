7e784c5b940da793aa001f250f209f7c64f6f747
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-01 17:39:18 +0000

    add base-config package

---
+++ base-config/package.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh ../.package-env.sh
+port='base-config'
+version='1.0'
+deps=""
+source_code="https://github.com/define-os/ports/raw/main/base-config/base-config.tgz"
+source_cd="."
+use_crosscompiler=false
+build() {
+    :
+}
+
+install() {
+    cp * / -r
+}
