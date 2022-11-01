0ba9ce4a00209afb6df7ccbfeddb43b948606e71
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-01 15:00:38 +0000

    add busybox

---
+++ busybox/package.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh ../.package-env.sh
+_port='busybox'
+port="${_port}"
+version='1.35.0'
+deps=""
+source_code="https://busybox.net/downloads/$_port-$version.tar.bz2"
+source_cd="$_port-$version"
+use_crosscompiler=true
+build() {
+    cp $PORTDIR/busybox.config .config
+    make -j$(nproc)
+}
+
+install() {
+    make CONFIG_PREFIX=$INSTALL_PATH install
+}
