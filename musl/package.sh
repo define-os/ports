1216f80105daa95725106e2b070dca56778ad0b7
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-01 12:45:18 +0000

    replace https to http

--- musl/package.sh
+++ musl/package.sh
@@ -2,7 +2,7 @@
 port='musl'
 version='1.2.3'
 deps=""
-source_code="https://musl.libc.org/releases/$port-$version.tar.gz"
+source_code="http://musl.libc.org/releases/$port-$version.tar.gz"
 source_cd="$port-$version"
 source_name="$port"
 use_crosscompiler=true
