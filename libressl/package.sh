771c1861ea8afb0a53d4428921f75ce592011703
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-06 11:29:19 +0300

    cmake  don't need

--- libressl/package.sh
+++ libressl/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh ../.package-env.sh
 port='libressl'
 version='3.6.1'
-deps="make cmake autoconf automake libtool"
+deps="make autoconf automake libtool"
 source_code="https://github.com/$port-portable/portable/archive/refs/tags/v${port}org-$version.tar.gz"
 source_cd="$port-$version"
 source_name="$port"
