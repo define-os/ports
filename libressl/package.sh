dd5da18d8f8e7058d0d6581284a0c7000dd771e2
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-06 11:55:25 +0300

    wtf

--- libressl/package.sh
+++ libressl/package.sh
@@ -3,7 +3,7 @@ port='libressl'
 version='3.6.1'
 deps="make autoconf automake libtool"
 source_code="https://github.com/$port-portable/portable/archive/refs/tags/v$version.tar.gz"
-source_cd="$port-$version"
+source_cd="portable-$version"
 source_name="$port"
 use_crosscompiler=true
 build() {
