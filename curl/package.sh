4006164698604a77ad7113b370d9e1408712939b
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-06 13:52:08 +0300

    fix

--- curl/package.sh
+++ curl/package.sh
@@ -2,7 +2,7 @@
 port='curl'
 version='7_86_0'
 deps="make autoconf libressl"
-source_code="https://github.com/$port/$port/archive/refs/tags/$port-$version.tar.xz"
+source_code="https://github.com/$port/$port/archive/refs/tags/$port-$version.tar.gz"
 source_cd="$port-$port-${version}"
 use_crosscompiler=true
 build() {
