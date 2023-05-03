49e130a1d19a70984e043295c030d6b089467044
aidgamesa <ya.ivanov1302@yandex.ru>
2023-05-03 10:32:17 +0000

    change gcc to tcc

---
+++ tcc/package.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/sh ../.package-env.sh
+port='tcc'
+version='29ae3ed'
+commit_long="4d5b83eec43598d6cd7949bccb41c8083"
+deps=""
+source_code="http://download.savannah.gnu.org/releases/tinycc/$port-$version$commit_long.tar.gz"
+source_cd="$port-$version"
+use_crosscompiler=true
+build() {
+    mkdir build
+    cd build
+
+    ../configure --prefix=$PREFIX
+    make -j$(nproc)
+}
+
+install() {
+    make DESTDIR=$INSTALL_PATH install 
+    ln -svr $INSTALL_PATH/bin/tcc $INSTALL_PATH/bin/gcc
+}
